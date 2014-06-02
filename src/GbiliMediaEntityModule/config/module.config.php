5377ede5e32b82600d0c2f179520c2182929cef6
Guillermo Devi <g@gbili.com>
2014-06-02 10:36:13 +0200

    moved in gbiliupmUploadFileHydrator implementation

--- src/GbiliMediaEntityModule/config/module.config.php
+++ src/GbiliMediaEntityModule/config/module.config.php
@@ -3,4 +3,6 @@ namespace GbiliMediaEntityModule;
 return array(
     'doctrine' => include __DIR__ . '/doctrine.config.php',
     'doctrine_event_listeners' => include __DIR__ . '/doctrine_event_listeners.config.php',
+    'service_manager' => include __DIR__ . '/service_manager.config.php',
+
 );
---
+++ src/GbiliMediaEntityModule/config/service_manager.config.php
@@ -0,0 +1,8 @@
+<?php
+namespace GbiliMediaEntityModule;
+
+return array(
+    'invokables' => array(
+        'gbiliupmUploadFileHydrator' => __NAMESPACE__ . '\Service\UploadFileHydrator',
+    ),
+);
